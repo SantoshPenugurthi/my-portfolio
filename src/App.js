d4182aa1678353aede5e75fd67a2a5ab8d7385d6
Santosh <n170425@rguktn.ac.in>
2022-07-31 15:46:41 +0530

    speed increase

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello Iam Santosh
-        </a>
       </header>
     </div>
   );
