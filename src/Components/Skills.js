71ef3ca6886f429f4901f3099ad45290a2dee5a8
Santosh <n170425@rguktn.ac.in>
2022-09-25 00:37:26 +0530

    skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -8,56 +8,57 @@ class Skills extends Component {
             >
                 <h1 style={{fontWeight:'bold',color:'#fff',paddingBottom:10}}>Skills</h1>
                 <div style={{display:'flex',flex:1,flexDirection:'row'}}>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,width:"30%"}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Programming Languages:</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>C</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>C++</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Python</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Java</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>JavaScript</h5></div>
+                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,width:"25%"}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Programming Languages:</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>C</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>C++</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>Python</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>Java</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>JavaScript</h5></div>
                 </div>
 
                 <div style={{display:'flex',flex:1,flexDirection:'row'}}>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,width:"30%"}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Web Technologies:</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>HTML</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>CSS</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>JavaScript</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>BootStrap</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Express</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>React</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Node.js</h5></div>
+                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,width:"25%"}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Web Technologies:</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>HTML</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>CSS</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>JavaScript</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>BootStrap</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>Express</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>React</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>Node.js</h5></div>
                 </div>
 
                 <div style={{display:'flex',flex:1,flexDirection:'row'}}>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,width:"30%"}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Mobile Technologies:</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>React</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>React Native</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Expo(CLI)</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Node.js</h5></div>
+                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,width:"25%"}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Mobile Technologies:</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>React</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>React Native</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>Expo(CLI)</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>Node.js</h5></div>
                 </div>
 
                 <div style={{display:'flex',flex:1,flexDirection:'row'}}>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,width:"30%"}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Databases:</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>SQL</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>MySQL</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>MongoDB</h5></div>
+                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,width:"25%"}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Databases:</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>SQL</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>MySQL</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>MongoDB</h5></div>
                 </div>
 
                 <div style={{display:'flex',flex:1,flexDirection:'row'}}>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,width:"30%"}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Other Technologies:</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Machine Learning</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Data Science</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>NLP</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>IoT</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Cyber Security</h5></div>
+                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,width:"25%"}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Other Technologies:</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>Machine Learning</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>Data Science</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>NLP</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>IoT</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>Cyber Security</h5></div>
                 </div>
 
                 <div style={{display:'flex',flex:1,flexDirection:'row'}}>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,width:"30%"}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Tools:</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Ms Office</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Photoshop</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Illustrator</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Premier Pro</h5></div>
-                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>After Effects</h5></div>
+                    <div style={{backgroundColor:"#35d1e6",padding:5,margin:5,paddingRight:20,paddingLeft:20,width:"25%"}}><h5 style={{color:"#040a1e",fontWeight:'bold'}}>Tools:</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>Ms Office</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>Photoshop</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>Illustrator</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>Premier Pro</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>After Effects</h5></div>
+                    <div style={{padding:5,margin:5,paddingRight:20,paddingLeft:20,borderRadius:50,borderColor:'#35d1e6',borderWidth:3,borderStyle:'solid'}}><h5 style={{color:"#35d1e6",fontWeight:'bold'}}>Canva</h5></div>
                 </div>
             </div>
         );
