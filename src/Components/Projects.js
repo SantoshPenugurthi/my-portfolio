489791e5d918f071ba1998bfd41058513ad3c89f
Santosh <n170425@rguktn.ac.in>
2022-09-25 12:39:05 +0530

    api

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -60,6 +60,60 @@ class Projects extends Component {
 
                     <div style={{display:'flex',flex:1,flexDirection:'column',borderWidth:7,borderColor:"#35d1e6",borderStyle:'solid',margin:5}}>
                         <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:25,color:"#fff",padding:20,alignItems:'center',justifyContent:'center'}}>Data Cleaning</h2>
+                        </div>
+                        <div style={{display:'flex',height:200,backgroundColor:"#35d1e6",alignItems:'center',justifyContent:'center',borderWidth:3,borderColor:"#35d1e6",borderStyle:'solid',marginLeft:10,marginRight:10}}>
+                            <h2 style={{fontWeight:'bold',fontSize:25,color:"#fff"}}>Image</h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,paddingTop:30}}>Duration &emsp;&nbsp;&nbsp;: 1 Month</h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Team Size &emsp;: 3</h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Role Played &nbsp;: Programmer</h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#fff",paddingLeft:10,paddingTop:20}}>Skills Used : Python, Data Analysis</h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#35d1e6",paddingLeft:10,paddingTop:10}}>Project Source : &nbsp;<a href="https://github.com/SantoshPenugurthi/Data_Cleaning" style={{color:"#35d1e6"}}> Github Link</a></h2>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div
+                    className="projects"
+                >
+                    
+                    <div style={{display:'flex',flex:1,flexDirection:'column',borderWidth:7,borderColor:"#35d1e6",borderStyle:'solid',margin:5}}>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:25,color:"#fff",padding:20,alignItems:'center',justifyContent:'center'}}>Data Scrapping</h2>
+                        </div>
+                        <div style={{display:'flex',height:200,backgroundColor:"#35d1e6",alignItems:'center',justifyContent:'center',borderWidth:3,borderColor:"#35d1e6",borderStyle:'solid',marginLeft:10,marginRight:10}}>
+                            <h2 style={{fontWeight:'bold',fontSize:25,color:"#fff"}}>Image</h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,paddingTop:30}}>Duration &emsp;&nbsp;&nbsp;: 1 Month</h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Team Size &emsp;: 3</h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Role Played &nbsp;: Programmer</h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#fff",paddingLeft:10,paddingTop:20}}>Skills Used : Python, Data Analysis, API</h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#35d1e6",paddingLeft:10,paddingTop:10}}>Project Source : &nbsp;<a href="https://github.com/SantoshPenugurthi/Data_Scraping" style={{color:"#35d1e6"}}> Github Link</a></h2>
+                        </div>
+                    </div>
+
+                    <div style={{display:'flex',flex:1,flexDirection:'column',borderWidth:7,borderColor:"#35d1e6",borderStyle:'solid',margin:5}}>
+                        <div>
                             <h2 style={{fontWeight:'bold',display:'flex',fontSize:25,color:"#fff",padding:20,alignItems:'center',justifyContent:'center'}}>Smart Dustbin using IoT</h2>
                         </div>
                         <div style={{display:'flex',height:200,backgroundColor:"#35d1e6",alignItems:'center',justifyContent:'center',borderWidth:3,borderColor:"#35d1e6",borderStyle:'solid',marginLeft:10,marginRight:10}}>
@@ -79,58 +133,6 @@ class Projects extends Component {
                         </div>
                         <div>
                             <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#35d1e6",paddingLeft:10,paddingTop:10}}>Project Source : &nbsp;<a href="https://github.com/SantoshPenugurthi/Smart_Dustbin_Using_IoT" style={{color:"#35d1e6"}}> Github Link</a></h2>
-                        </div>
-                    </div>
-                </div>
-
-                <div
-                    className="projects"
-                >
-                    <div style={{display:'flex',flex:1,flexDirection:'column',borderWidth:7,borderColor:"#35d1e6",borderStyle:'solid',margin:5}}>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:25,color:"#fff",padding:20,alignItems:'center',justifyContent:'center'}}>Data Cleaning</h2>
-                        </div>
-                        <div style={{display:'flex',height:200,backgroundColor:"#35d1e6",alignItems:'center',justifyContent:'center',borderWidth:3,borderColor:"#35d1e6",borderStyle:'solid',marginLeft:10,marginRight:10}}>
-                            <h2 style={{fontWeight:'bold',fontSize:25,color:"#fff"}}>Image</h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,paddingTop:30}}>Duration &emsp;&nbsp;&nbsp;: 1 Month</h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Team Size &emsp;: 3</h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Role Played &nbsp;: Programmer</h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#fff",paddingLeft:10,paddingTop:20}}>Skills Used : Python</h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#35d1e6",paddingLeft:10,paddingTop:10}}>Project Source : &nbsp;<a href="https://github.com/SantoshPenugurthi/Data_Cleaning" style={{color:"#35d1e6"}}> Github Link</a></h2>
-                        </div>
-                    </div>
-                    
-                    <div style={{display:'flex',flex:1,flexDirection:'column',borderWidth:7,borderColor:"#35d1e6",borderStyle:'solid',margin:5}}>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:25,color:"#fff",padding:20,alignItems:'center',justifyContent:'center'}}>Data Scrapping</h2>
-                        </div>
-                        <div style={{display:'flex',height:200,backgroundColor:"#35d1e6",alignItems:'center',justifyContent:'center',borderWidth:3,borderColor:"#35d1e6",borderStyle:'solid',marginLeft:10,marginRight:10}}>
-                            <h2 style={{fontWeight:'bold',fontSize:25,color:"#fff"}}>Image</h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,paddingTop:30}}>Duration &emsp;&nbsp;&nbsp;: 1 Month</h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Team Size &emsp;: 3</h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Role Played &nbsp;: Programmer</h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#fff",paddingLeft:10,paddingTop:20}}>Skills Used : Python</h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#35d1e6",paddingLeft:10,paddingTop:10}}>Project Source : &nbsp;<a href="https://github.com/SantoshPenugurthi/Data_Scraping" style={{color:"#35d1e6"}}> Github Link</a></h2>
                         </div>
                     </div>
 
