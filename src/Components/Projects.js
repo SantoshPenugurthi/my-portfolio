55811b8bebae947a4db91aa7f30da0a5ba687541
Santosh <n170425@rguktn.ac.in>
2023-01-25 16:30:42 +0530

    major project update

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,6 +10,30 @@ class Projects extends Component {
                 <div
                     className="projects"
                 >
+
+                    <div style={{display:'flex',flex:1,flexDirection:'column',borderWidth:7,borderColor:"#35d1e6",borderStyle:'solid',margin:5}}>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:25,color:"#fff",padding:20,alignItems:'center',justifyContent:'center'}}>AI Integ. Blockchain for Auton.</h2>
+                        </div>
+                        <div style={{display:'flex',height:200,backgroundColor:"#35d1e6",alignItems:'center',justifyContent:'center',borderWidth:3,borderColor:"#35d1e6",borderStyle:'solid',marginLeft:10,marginRight:10}}>
+                            <h2 style={{fontWeight:'bold',fontSize:25,color:"#fff"}}>Image</h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,paddingTop:30}}>Duration &emsp;&nbsp;&nbsp;: 12 Months</h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Team Size &emsp;: 5</h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Role Played &nbsp;: Team Leader, Developer</h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#fff",paddingLeft:10,paddingTop:20}}>Skills Used : AI, Blockchain, IoT </h2>
+                        </div>
+                        <div>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#35d1e6",paddingLeft:10,paddingTop:10}}>Project Source : &nbsp;<a href="https://github.com/SantoshPenugurthi/" style={{color:"#35d1e6"}}> Github Link</a></h2>
+                        </div>
+                    </div>
                     <div style={{display:'flex',flex:1,flexDirection:'column',borderWidth:7,borderColor:"#35d1e6",borderStyle:'solid',margin:5}}>
                         <div>
                             <h2 style={{fontWeight:'bold',display:'flex',fontSize:25,color:"#fff",padding:20,alignItems:'center',justifyContent:'center'}}>My Portfolio</h2>
@@ -48,7 +72,7 @@ class Projects extends Component {
                             <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Team Size &emsp;: 5</h2>
                         </div>
                         <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Role Played &nbsp;: Developer, Tester</h2>
+                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Role Played &nbsp;:Team Lead, Developer, Tester</h2>
                         </div>
                         <div>
                             <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#fff",paddingLeft:10,paddingTop:20}}>Skills Used : Python, DAA, DS, Math</h2>
@@ -57,6 +81,12 @@ class Projects extends Component {
                             <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#35d1e6",paddingLeft:10,paddingTop:10}}>Project Source : &nbsp;<a href="https://github.com/SantoshPenugurthi/Topic-Modelling-on-Telugu-English-Code-mixed-data-using-LDA-" style={{color:"#35d1e6"}}> Github Link</a></h2>
                         </div>
                     </div>
+                    
+                </div>
+
+                <div
+                    className="projects"
+                >
 
                     <div style={{display:'flex',flex:1,flexDirection:'column',borderWidth:7,borderColor:"#35d1e6",borderStyle:'solid',margin:5}}>
                         <div>
@@ -81,12 +111,6 @@ class Projects extends Component {
                             <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#35d1e6",paddingLeft:10,paddingTop:10}}>Project Source : &nbsp;<a href="https://github.com/SantoshPenugurthi/Data_Cleaning" style={{color:"#35d1e6"}}> Github Link</a></h2>
                         </div>
                     </div>
-                    
-                </div>
-
-                <div
-                    className="projects"
-                >
                     
                     <div style={{display:'flex',flex:1,flexDirection:'column',borderWidth:7,borderColor:"#35d1e6",borderStyle:'solid',margin:5}}>
                         <div>
@@ -136,29 +160,7 @@ class Projects extends Component {
                         </div>
                     </div>
 
-                    <div style={{display:'flex',flex:1,flexDirection:'column',borderWidth:7,borderColor:"#35d1e6",borderStyle:'solid',margin:5}}>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:25,color:"#fff",padding:20,alignItems:'center',justifyContent:'center'}}>Upcoming..[Major Project]</h2>
-                        </div>
-                        <div style={{display:'flex',height:200,backgroundColor:"#35d1e6",alignItems:'center',justifyContent:'center',borderWidth:3,borderColor:"#35d1e6",borderStyle:'solid',marginLeft:10,marginRight:10}}>
-                            <h2 style={{fontWeight:'bold',fontSize:25,color:"#fff"}}>Image</h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,paddingTop:30}}>Duration &emsp;&nbsp;&nbsp;: 12 Months</h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Team Size &emsp;: 3</h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:20,color:"#fff",paddingLeft:10,}}>Role Played &nbsp;: Will be Updated</h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#fff",paddingLeft:10,paddingTop:20}}>Skills Used : Will be Updated </h2>
-                        </div>
-                        <div>
-                            <h2 style={{fontWeight:'bold',display:'flex',fontSize:15,color:"#35d1e6",paddingLeft:10,paddingTop:10}}>Project Source : &nbsp;<a href="https://github.com/SantoshPenugurthi/" style={{color:"#35d1e6"}}> Github Link</a></h2>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
         );
